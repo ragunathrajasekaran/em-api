c08c7ccf91d6fcf2566bf0aa28fdb857df442a2d
Ragunath <ragunathrajasekaran@gmail.com>
2019-10-16 18:43:31 +0530

    CrossOrigin disabled

--- src/main/java/com/rrr/expense/manager/emapi/controllers/AccountController.java
+++ src/main/java/com/rrr/expense/manager/emapi/controllers/AccountController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AccountController {
 
     private AccountRepository accountRepository;
